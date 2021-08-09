2253d004083731c0e9484a974c318de16e563a1f
Anirban Kopty <kopty.anirban@gmail.com>
2021-08-09 20:31:02 +0530

    Assignment 2 added

---
+++ NPTEL/Week 2/Assignment 2/Rand.f90
@@ -0,0 +1,50 @@
+SUBROUTINE Random_Integer(dim, int_rand)
+    implicit none
+    
+    INTEGER :: dim, i
+    INTEGER :: int_rand(dim)
+    REAL*8 :: rand_array(dim)
+
+    ! Calling Random Function
+    CALL Random_Gen(dim, rand_array)
+    ! PRINT *, rand_array(1:10)             ! Before rounding
+
+    int_rand = NINT(rand_array)
+    ! PRINT *, int_rand(1:10)               ! After rounding
+
+    ! Changing all the 0s to -1s
+    do i = 1, dim
+        if ( int_rand(i) == 0 ) then
+            int_rand(i) = -1
+        end if
+    end do
+    ! PRINT *, int_rand(1:10)                ! -1 and 1 random numbers
+
+end SUBROUTINE Random_Integer
+
+! Made a SUBROUTINE for this so that I can call the same Random multiple times
+!to get new Random Integer sets
+
+program Rand
+    implicit none
+
+    INTEGER, PARAMETER :: dim=10000, sum_dim = 100000
+    INTEGER :: n,i, int_rand(dim), summ_r(sum_dim)=0
+
+
+    do n=1, sum_dim
+        CALL Random_Integer(dim, int_rand)
+        ! PRINT *, int_rand(1:10)
+        do i = 1, dim
+            summ_r(n) = summ_r(n) + int_rand(i)
+        end do
+    end do
+
+    ! PRINT *, summ_r(1:20)
+
+    OPEN(UNIT=15, FILE="histogram.dat", STATUS="REPLACE")
+    do n = 1, sum_dim
+        WRITE(15,*) summ_r(n)
+    end do
+
+end program Rand
---
+++ NPTEL/Week 2/Assignment 2/Trapezoidal.f90
@@ -0,0 +1,39 @@
+! I = \int_0^{1/2} \sqrt{1-t^2} dt with 100 gridpoints in double precision floats
+
+program trapezoidal
+    implicit none
+    
+    REAL*8 :: h, Integrand, sum                     ! h = step size, sum = Integrated result
+    INTEGER :: i, n=10000                           ! i = Iterative, n = #Gridpoints
+    REAL*8, ALLOCATABLE :: t(:)
+
+    ALLOCATE(t(n))
+
+    h = 0.5d0 / (REAL(n-1))                         ! Step size
+
+    t(1) = 0.d0                                     ! Starting limit
+    ! Filling the variable values
+    do i = 2, n
+        t(i) = t(i-1) + h
+    end do
+
+    ! Integration time
+    sum = Integrand(t(1)) + Integrand(t(n))
+    do i = 1, n-1
+        sum = sum + 2 * Integrand(t(i))
+    end do
+    sum = sum * h/2.d0
+
+    ! RESULT
+    PRINT *, "The result to the integration I = \int_0^{1/2} \sqrt{1-t^2} dt with 100 gridpoints in double precision floats is :"
+    PRINT *, sum
+
+end program trapezoidal
+
+REAL*8 FUNCTION Integrand(t)
+    implicit none
+
+    REAL*8 :: t
+
+    Integrand = SQRT(1-t**2)
+END FUNCTION Integrand
