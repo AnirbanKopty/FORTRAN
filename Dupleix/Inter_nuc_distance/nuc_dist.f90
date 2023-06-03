4c68e9902e66c44622245074b4462de888a2ed5c
Anirban Kopty <kopty.anirban@gmail.com>
2023-06-04 01:59:34 +0530

    correct renaming from 'mol' to 'nuc'

--- Dupleix/Inter_nuc_distance/nuc_dist.f90
+++ Dupleix/Inter_nuc_distance/nuc_dist.f90
@@ -8,24 +8,26 @@ program mol_dist
     INTEGER :: J(dim) = (/(itr, itr=0,dim-1,1)/) , gJ       ! using constructor (/ /) (basically list comprehension) in fortran, it can only handle integer steps
 
     !? function evaluation for plotting
-    OPEN(UNIT=14, FILE='mol_plot.dat', STATUS='replace')
-
+    OPEN(UNIT=14, FILE='nuc_plot.dat', STATUS='replace')
+    
     do itr = 1, dim
         gJ = 2*J(itr)+1
         EJ = B*h*c * J(itr)*(J(itr)+1)
-
+        
         WRITE(14, *) J(itr), gJ * exp(- EJ/ (k*T))
     end do
-
+    
     CLOSE(14)
-
+    
     !? Finding Inter-nuclear distance R
     ! used the formula of B to get I, then R = sqrt(I/mu)
     mu = m1*m2/(m1+m2) * 1.66*10.0d0**(-27)
     I = h/(8*PI**2 * c* B)
     R = sqrt(I/mu)              ! in meter
     R = R * 10.0d0**(12)
-
-    PRINT "(A, f0.4, A, f0.4, A)", "The Inter-nuclear distance, given B = ", B, " per m is, R = ", R, " pm"
+    
+    OPEN(UNIT=15, FILE='param.dat', STATUS='replace')
+    WRITE (15,"(A, f0.4, A, f0.4, A)") "The Inter-nuclear distance, given B = ", B, " per m is, R = ", R, " pm"
+    CLOSE(15)
 
 end program mol_dist
